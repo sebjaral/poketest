adad4082a00ee4fda7c74cbccfba001a92d4618a
sebjaral <seb.jaral@duocuc.cl>
2022-03-03 10:25:14 -0300

    Actualización de código que ya no se está utilizando

--- src/App.js
+++ src/App.js
@@ -7,19 +7,16 @@ import background from './background.png';
 import Searchbar from './Searchbar';
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
+ 
 
-  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-     setPokemon(res.data.results.map(p => p.name))
-  })
-
+ 
   return (
     <div>
       <Navbar/>
       <div>
         <Searchbar/>
         <div style={{ backgroundImage: `url(${background})` }}>
-        <Pokedex pokemon = {pokemon}/>
+       
       </div>
       </div>
     </div>
